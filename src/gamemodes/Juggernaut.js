ea19492084c8582d90ad8a4a3ea126c3dc6edd35
scholtes <scholtes.garrett@gmail.com>
2016-03-20 15:10:08 -0400

    Juggernaut - added Juggernaut player feature

--- src/gamemodes/Juggernaut.js
+++ src/gamemodes/Juggernaut.js
@@ -16,6 +16,52 @@ Juggernaut.prototype = new Experimental();
 
 Juggernaut.prototype.onServerInit = function(gameServer) {
     Experimental.prototype.onServerInit.call(this, gameServer);
+    var PlayerTracker = require('../PlayerTracker');
+    var Commands = require('../modules/CommandList');
+
+    var oldHelp = Commands.list.help;
+    Commands.list.help = function(gameServer, split) {
+        oldHelp.call(gameServer, split);
+        console.log("[Console] juggernaut : set or view player's juggernautness");
+        console.log("[Console] ====================================================");
+    };
+
+    Commands.list.juggernaut = function(gameServer,split) {
+        // Validation checks
+        var id = parseInt(split[1]);
+        if (isNaN(id)) {
+            console.log("[Console] Please specify a valid player ID!");
+            console.log("[Console] key : " + gameServer.juggernautID);
+            return;
+        }
+
+        // Get player
+        var client;
+        for (var i in gameServer.clients) {
+            if (gameServer.clients[i].playerTracker.pID == id) {
+                client = gameServer.clients[i].playerTracker;
+                break;
+            }
+        }
+
+        if(client) {
+            if(split.length == 2) {
+                // tell if juggernaut
+                console.log("[Console] " + client.juggernaut);
+            } else if(split.length >= 2) {
+                var makeJugg = (split[2] === 'true');
+                if(makeJugg) {
+                    client.makeJuggernaut();
+                    console.log("[Console] Player " + id + " made juggernaut");
+                } else {
+                    client.makeNotJuggernaut();
+                    console.log("[Console] Player " + id + " made non-juggernaut");
+                }
+            }
+        } else {
+            console.log("[Console] No player with that ID!");
+        }
+    };
 
     Virus.prototype.virusFeedAmount = null;
 
@@ -71,7 +117,16 @@ Juggernaut.prototype.onServerInit = function(gameServer) {
         return this.virusFeedAmount;
     };
 
+    var oldVirusOnConsume = Virus.prototype.onConsume;
+    Virus.prototype.onConsume = function(consumer, gameServer) {
+        if(consumer.owner.juggernaut) {
+            consumer.owner.makeNotJuggernaut();
+        }
+        oldVirusOnConsume.call(this, consumer, gameServer);
+    };
+
     PlayerCell.prototype.onAutoMove = function(gameServer) {
+        this.juggernautable = false;
         if (this.owner.cells.length == 2) {
             // Check for viruses
             var v = gameServer.getNearestVirus(this);
@@ -104,7 +159,124 @@ Juggernaut.prototype.onServerInit = function(gameServer) {
         Cell.prototype.calcMovePhys.call(this, config);
         if (this.moveEngineTicks > 0) {
             this.onAutoMove(this.gameServer);
+        } else if (!this.juggernautable) {
+            this.juggernautable = true;
         }
     };
+
+    PlayerCell.prototype.juggernautable = true;
+
+    var oldPlayerCellOnConsume = PlayerCell.prototype.onConsume;
+    PlayerCell.prototype.onConsume = function(consumer, gameServer) {
+        var thisOwnerWasJuggernaut = this.owner.juggernaut;
+        this.owner.makeNotJuggernaut();
+        if (thisOwnerWasJuggernaut && consumer.juggernautable) {
+            consumer.addMass(-2*this.mass);
+            if (consumer.mass < 10) {
+                consumer.mass = 10;
+            }
+            Virus.prototype.onConsume.call(this, consumer, gameServer);
+            // Times 2 because Virus.prot.onConsume adds the mass first
+        } else {
+            oldPlayerCellOnConsume.call(this, consumer, gameServer);
+        }
+    };
+
+    gameServer.hasJuggernaut = false;
+
+    var oldSplitCells = Object.getPrototypeOf(gameServer).splitCells;
+    Object.getPrototypeOf(gameServer).splitCells = function(client) {
+        if (client.juggernaut) {
+            return;
+        }
+        oldSplitCells.call(this, client);
+    };
+
+    var oldSpawnPlayer = Object.getPrototypeOf(gameServer).spawnPlayer;
+    Object.getPrototypeOf(gameServer).spawnPlayer = function(player, pos, mass) {
+        oldSpawnPlayer.call(this, player, pos, mass);
+
+        // Make juggernaut if they have the key
+        if (player.name === this.juggernautID) {
+            player.makeJuggernaut();
+            this.juggernautID = this.makeID();
+            console.log("[Console] New juggernaut key: " + this.juggernautID);
+        }
+
+        // Make juggernaut if there is no juggernaut in play (except admin)
+        // The randomness is so that a player who dies and immediately
+        // rejoins isn't stuck as juggernaut forever. The constant
+        // is arbitrary
+        if(Math.random() < 0.333 && !this.hasJuggernaut) {
+            player.makeJuggernaut();
+        }
+    };
+
+    Object.getPrototypeOf(gameServer).makeID = function() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for(var i = 0; i < 8; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return text;
+    };
+
+    gameServer.juggernautID = gameServer.makeID();
+
+    PlayerTracker.prototype.juggernaut = false;
+    PlayerTracker.prototype.oldName = "";
+
+    PlayerTracker.prototype.setName = function(name) {
+        this.name = name;
+        this.oldName = name;
+    };
+
+    PlayerTracker.prototype.makeJuggernaut = function() {
+        this.juggernaut = true;
+        this.gameServer.hasJuggernaut = true;
+        this.oldName = this.name;
+        this.name = "";
+        var color = {
+            r: 104 + Math.floor(48*Math.random()),
+            g: 0,
+            b: 40 + Math.floor(48*Math.random())
+        };
+        this.setColor(color);
+        for(var i = 0; i < this.cells.length; i++) {
+            this.cells[i].spiked = 1;
+            this.cells[i].color = color;
+        }
+    };
+
+    PlayerTracker.prototype.makeNotJuggernaut = function() {
+        if (this.juggernaut) {
+            this.juggernaut = false;
+            this.gameServer.hasJuggernaut = false;
+            this.name = this.oldName;
+            for(var i = 0; i < this.cells.length; i++) {
+                this.cells[i].spiked = 0;
+            }
+        }
+    }; 
+
+    var oldPlayerTrackerUpdate = PlayerTracker.prototype.update;
+    PlayerTracker.prototype.update = function() {
+        if (this.disconnect === 0) {
+            this.makeNotJuggernaut();
+        }
+        oldPlayerTrackerUpdate.call(this);
+    };
+
+    // TODO: if Mother Cell is put in its own file, redo the logic below
+    // Make sure the juggernaut is removed no matter what (e.g., mother cell)
+    var oldRemoveNode = Object.getPrototypeOf(gameServer).removeNode;
+    Object.getPrototypeOf(gameServer).removeNode = function(check) {
+        if(check.cellType === 0 && check.owner.juggernaut) {
+            check.owner.makeNotJuggernaut();
+        }
+        oldRemoveNode.call(this, check);
+    }
 
 }
